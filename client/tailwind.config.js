e45b9f58700b90b52fe93647bc9a13c6f66afce5
Artem Mazin <minima1s@yandex.ru>
2023-06-08 12:51:04 +0300

    feat: создать структуру клиента

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import withMT from '@material-tailwind/react/utils/withMT';
+
+module.exports = withMT({
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
